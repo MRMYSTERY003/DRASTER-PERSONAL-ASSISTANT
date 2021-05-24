6dab37dda8181c0671274c2393a53f09aac4e4cc
Sriram <80098044+MRMYSTERY003@users.noreply.github.com>
2021-05-24 10:28:43 +0530

    entering to comments to the program

--- alpha2.0.py
+++ alpha2.0.py
@@ -1,3 +1,4 @@
+#improyting all the required modules
 import speech_recognition as sr
 import pyttsx3 as tt
 import datetime as date
@@ -18,30 +19,28 @@ import datetime
 import subprocess
 from pygame import mixer 
 
-
+#initilizing the required modules
 mouse = Controller()
-
-
-
-
-
-
 en = tt.init()
 vo = en.getProperty('voices')
 en.setProperty('voice', vo[3].id)
 en.setProperty('rate', 160)
-woappid = '9W52T2-Y2W25K786Y'
+go = 'https://www.google.com'
+yt = 'https://www.youtube.com'
+insta = 'https://www.instagram.com'
+wp = 'https://web.whatsapp.com/'
+woappid = '*****YOUR WOLFRAMALPHA API KEY HERE*****'
 
 
+#defining the required funcitons
 
-
-def speak(audio):
+def speak(audio):#used to convert the text to audio
     en.say(audio)
     en.runAndWait()
 
 
 
-def take_command():
+def take_command():#convert the audio to text
     try:
 
         r = sr.Recognizer()
@@ -56,9 +55,10 @@ def take_command():
     except:
         print('unable to recognize voice')
         return 'sr030'
-            
+ 
 
-def get_sysinfo():
+
+def get_sysinfo():#used to get the system cpu usage
     info = []
     cpufreq = psutil.cpu_freq()
     info.append(f"Current Frequency of the CPU is: {cpufreq.current:.2f} Megahertz")
@@ -69,15 +69,8 @@ def get_sysinfo():
     return info
 
 
-go = 'https://www.google.com'
-yt = 'https://www.youtube.com'
-insta = 'https://www.instagram.com'
-wp = 'https://web.whatsapp.com/'
 
-
-
-
-def browser(site):
+def browser(site):#used to open the browse and search in google
     while True:   
         if ('google' in site):
             speak('openning google')
@@ -145,57 +138,29 @@ def wish_me():
     
 
 
-
-
-def username():
-    speak('what should i call you sir')
-    uname = take_command()
-    cu = 'Welcome Mr.'+uname
-    speak('welcome Mister')
-    speak(uname)
-    columns = shutil.get_terminal_size().columns
-    print('#######################'.center(columns))
-    print(cu.center(columns))
-    print("#######################".center(columns))
-    speak('How can i help you sir?')
-
-def bgm():
-    
-        mixer.init() 
-
-        
-
-        mixer.music.load("C:\\Users\\kamat\\Desktop\\python projrcts\\pythonProject\\pubg.mp3") 
-
-        mixer.music.set_volume(0.015) #from 0.0 to 1.0
-
-        mixer.music.play() 
-        time.sleep(29)
-
-
 def send_email(to, content):
     server = smtplib.SMTP('smtp.gmail.com',587)
     server.ehlo()
     server.starttls()
-    server.login('sriramanand003@gmail.com','stranger003')
-    server.sendmail('sriramanand003@gamil.com',to,content)
+    server.login('****YOUR MAIL ID****','****YOUR MAIL PASSWORD****')
+    server.sendmail('****YOUR MAIL ID****',to,content)
     server.close()
 
 
 wish_me()
 
-
+#program starts here
 if __name__ =='__main__':
-    lambda : os.system('cls')
+    lambda : os.system('cls')# clearing the terminal
 
     mrun = True
     while mrun:
 
 
-        task = take_command().lower()
+        task = take_command().lower()#getting the task from the user in voice form 
         print(task)
         
-        if 'wikipedia' in task:
+        if 'wikipedia' in task:     #wikipedia searches
             speak('analysing your command..')
             speak('Searching Wikipedia...')
             info = task.replace('wikipedia', '')
@@ -208,12 +173,13 @@ if __name__ =='__main__':
                 speak('not found in wikipedia , searching on google')
                 speak('these are the results found on google')
                 pw.search(info)
+                
         elif 'send an email to me' in task:
             speak('analysing your command..')
             try:
                 speak('What should I send ')
                 content = take_command()
-                to = 'sriramanand23@gmail.com'
+                to = '****YOUR MAIL ID****'
                 speak('sending email to '+ to)
                 send_email(to, content)
                 speak('email has been sent succesfully sir..')
@@ -221,7 +187,7 @@ if __name__ =='__main__':
                 print(e)
                 speak('i am not able to send this email')
 
-        elif 'send an email' in task:
+        elif 'send an email' in task:       #send mail to the sepcified emali in the termianl
             speak('analysing your command..')
             try:
                 speak('whom should i send')
@@ -265,7 +231,7 @@ if __name__ =='__main__':
                     speak('try again')
                     continue
             
-        elif "open youtube" in task:
+        elif "open youtube" in task:    #opens youtube and play the specified sond, try sepaking open youtube and play the song needed
             speak('analysing your command..')
             speak('openning youtube..\n')
             if 'play' in task:
@@ -278,31 +244,31 @@ if __name__ =='__main__':
             else:
                 wbb.open('youtube.com')
 
-        elif 'open google' in task:
+        elif 'open google' in task:     #open google
             speak('analysing your command..')
             speak('hear you go to google')
             wbb.open('google.com')
 
-        elif 'open instagram' in task:
+        elif 'open instagram' in task:      #open instagram
             speak('analysing your command..')
             speak('openning insta')
             wbb.open('instagram.com')
 
-        elif 'exit' in task:
+        elif 'exit' in task:        # to exit the loop
             speak('Thanks for giving me your time')
             speak('see you later')
             subprocess.call(['taskkill','/F','/IM','Rainmeter.exe'])
             mrun = False
 
-        elif 'song' in task or ('music' in task) or('on pc' in task) or ('in pc' in task):
+        elif 'song' in task or ('music' in task) or('on pc' in task) or ('in pc' in task):      #to play a random song from the specified folder
             speak('analysing your command..')
-            i = random.randint(1,11)
+            i = random.randint(1,'***number of songs +1')
             speak('playin a song on pc')
-            path = "S:\\alpha_music"
+            path = "****folder path containing songs"
             song_list = os.listdir(path)
             os.startfile(os.path.join(path, song_list[i]))
         
-        elif 'time' in task:
+        elif 'time' in task:    # to know the time
             time = datetime.datetime.now().strftime('%I %M %p')
             print("the current time is ",time)
             speak("the current time is " + time)
@@ -310,26 +276,26 @@ if __name__ =='__main__':
         elif ('do you know tamil' in task):
             speak('enaku tamil theriyathu')
 
-        elif ('open notepad' in task) or ('start notepad' in task):
+        elif ('open notepad' in task) or ('start notepad' in task):     #open notepad
             speak('opening notepad')
             os.startfile('C:\\Windows\\system32\\notepad.exe')
 
-        elif ('close notepad' in task) or ( 'exit notepad' in task):
+        elif ('close notepad' in task) or ( 'exit notepad' in task):        #close notepad
             speak('terminating notepad')
             subprocess.call(['taskkill','/F','/IM','notepad.exe']) 
 
-        elif ('open paint' in task) or ('open mspaint' in task) or ('start paint' in task):
+        elif ('open paint' in task) or ('open mspaint' in task) or ('start paint' in task):        #open paint
             speak('opening microsoft paint')
             os.startfile('C:\\Windows\\system32\\mspaint.exe')
         
-        elif ('close paint' in task) or ('terminate paint' in task) or ('exit paint' in task):
+        elif ('close paint' in task) or ('terminate paint' in task) or ('exit paint' in task):        #closepaint
             speak('terminating microsoft paint') 
             subprocess.call(['taskkill','/F','/IM','mspaint.exe']) 
 
         elif 'who are you' in task:
             speak('i am draster , i am your personal assestant . i help you playing youtube videos, musics, getting informations , google searches, excetra')
         
-        elif ('what ' in task) or('who' in task) or ('how' in task) or ('when' in task) or ('where' in task) or ('why' in task) or ('tell' in task):
+        elif ('what ' in task) or('who' in task) or ('how' in task) or ('when' in task) or ('where' in task) or ('why' in task) or ('tell' in task):    # to get informations
             speak('analysing your command..')
             try:
                 client = wo.Client(woappid)
